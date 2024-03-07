5e0d3ecbfdd7fad61f526bc3574af22f799c98ce
Elaraby218 <icpcnews5@gmail.com>
2024-03-07 23:44:05 +0200

    View for login and sign up done

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ThreeFriends.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Signup()
+        {
+            return View();
+        }
+    }
+}
