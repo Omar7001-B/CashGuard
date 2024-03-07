fcdace7650ab79c298f70f21e36e97093f65029c
Elaraby218 <icpcnews5@gmail.com>
2024-03-07 17:49:33 +0200

    Model Added 1- User table added 2- nugets packages added 3- Dbcontext calss added 4- Connection string to json file added 5- retrieve conncection string form the json file 6- add migration init 7- updata data base

---
+++ Models/Appdbcontxt.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace ThreeFriends.Models
+{
+    public class Appdbcontxt : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        public DbSet<User>Users { get; set; }
+
+        public Appdbcontxt(IConfiguration configuration):base()
+        {
+            _configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string constring = _configuration.GetConnectionString("DefaultConnection"); 
+            optionsBuilder.UseSqlServer(constring);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .Property(u => u.Password)
+                .IsRequired();
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace ThreeFriends.Models
+{
+    [Table("User")]
+    public class User
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column(TypeName = "int")]
+        public int Id { get; set; }
+        [Required]
+        public string User_Name { get; set; }
+        [Required]
+        public string First_Name { get; set; }
+        [Required]
+        public string Last_Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Phone_Number { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public DateTime Sign_Up_Date { get; set; }
+        public string? Bank_Account_ID {  get; set; }
+    }
+}
