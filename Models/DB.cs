5df120cb37230a47f12dd2fe7f9da3554d071551
Omar7001-B <Omar.20375710@compit.aun.edu.eg>
2024-03-08 19:47:40 +0200

    Created CB

---
+++ Models/DB.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ThreeFriends.Models
+{
+	public class DB:DbContext
+	{
+		public DbSet<User> Users { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(@"Data Source=(localdb)\ProjectModels;Initial Catalog=ThreeFriends;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+		}
+	}
+}
