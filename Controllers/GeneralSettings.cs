905d4089175f2f62af9781b9645126d23b465dc0
Elaraby218 <icpcnews5@gmail.com>
2024-04-28 02:04:16 +0300

    GeneralSettings Page Added (Controller need more implementation)

---
+++ Controllers/GeneralSettings.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ThreeFriends.Controllers
+{
+    public class GeneralSettings : Controller
+    {
+        // i can send class as binding butt the class is static ):
+        public IActionResult Index(DateTime DataRangeFrom ,DateTime DateRangeTo , string Currency , DateTime DeleteOldTo)
+        {
+            return View();
+        }
+    }
+}
