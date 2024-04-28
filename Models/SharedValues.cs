f195297177d8d743c6c9ba3a874f8dbfe7a125b0
Elaraby218 <icpcnews5@gmail.com>
2024-04-29 02:26:29 +0300

    General settings is done

--- Models/SharedValues.cs
+++ Models/SharedValues.cs
@@ -3,6 +3,9 @@
     public static class SharedValues
     {
         public static User CurUser = new User();
-   
+        public static DateTime DataRangeFrom = default(DateTime);
+        public static DateTime DataRangeTo = default(DateTime);
+        public static string Currency= null;
+        public static DateTime DeleteOldTo = default(DateTime);
     }
 }
