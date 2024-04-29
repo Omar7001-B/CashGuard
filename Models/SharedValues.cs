b07d33cfc48e65cb133e56ccc4292e46cd76bb27
Elaraby218 <icpcnews5@gmail.com>
2024-04-29 23:32:32 +0300

    Function SetDefaults added to set default values for this modle attributes

--- Models/SharedValues.cs
+++ Models/SharedValues.cs
@@ -5,7 +5,16 @@
         public static User CurUser = new User();
         public static DateTime DataRangeFrom = default(DateTime);
         public static DateTime DataRangeTo = default(DateTime);
-        public static string Currency= null;
+        public static string Currency = null;
         public static DateTime DeleteOldTo = default(DateTime);
+
+        public static void SetAllNull()
+        {
+            SharedValues.CurUser = new User();
+            SharedValues.DataRangeFrom = default(DateTime);
+            SharedValues.DataRangeTo = default(DateTime);
+            SharedValues.Currency = null;
+            SharedValues.DeleteOldTo = default(DateTime);
+        }
     }
 }
