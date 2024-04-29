7ba95329c650f98e5cdcf4e09e7e95eb196710fd
Elaraby218 <icpcnews5@gmail.com>
2024-04-29 23:34:38 +0300

    fix some bugs and ensure that work correctly

---
+++ Controllers/LogOutController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ThreeFriends.Models;
+
+namespace ThreeFriends.Controllers
+{
+    public class LogOutController : Controller
+    {
+        public IActionResult index()
+        {
+            SharedValues.SetAllNull();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
