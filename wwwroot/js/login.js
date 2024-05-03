8790d620ca1c75328a8072ff0823c18fd3120f54
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-05-03 19:14:07 +0300

    Fix some codes - now you cannot go back from the browser and when hit reload the page still in the same place

--- wwwroot/js/login.js
+++ wwwroot/js/login.js
@@ -1,6 +1,7 @@
 ﻿const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
+
 function previewPhoto(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
@@ -11,8 +12,6 @@ function previewPhoto(input) {
         reader.readAsDataURL(input.files[0]);
     }
 }
-
-
 
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
