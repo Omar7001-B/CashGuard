f53f4f239bdd28196e1c4115e4c94925a1c657f5
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-05-04 00:22:20 +0300

    Shared Value now hold only the CurUser GeneralSettings now have the General Settins basic attributes

---
+++ Models/GeneralSettings.cs
@@ -0,0 +1,27 @@
+﻿namespace ThreeFriends.Models
+{
+    public static class GeneralSettings
+    {
+        public static DateTime DataRangeFrom = default(DateTime);
+        public static DateTime DataRangeTo = default(DateTime);
+        public static string Currency = null;
+        public static DateTime DeleteOldTo = default(DateTime);
+
+        public static void SetAllNull()
+        {
+            GeneralSettings.DataRangeFrom = default(DateTime);
+            GeneralSettings.DataRangeTo = default(DateTime);
+            GeneralSettings.Currency = null;
+            GeneralSettings.DeleteOldTo = default(DateTime);
+        }
+
+        //public static void SetValues(DateTime DataRangeFrom, DateTime DateRangeTo, string Currency, DateTime DeleteOldTo)
+        //{
+
+        //    GeneralSettings.DataRangeFrom = DataRangeFrom;
+        //    GeneralSettings.DataRangeTo = DateRangeTo;
+        //    GeneralSettings.Currency = Currency;
+        //    GeneralSettings.DeleteOldTo = DeleteOldTo;
+        //}
+    }
+}
