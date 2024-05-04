807613d3f61ced515caf1c52de3ed01dfc632cef
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-05-04 18:04:31 +0300

    add general setting model to hold the data of the general setting

--- Models/GeneralSettings.cs
+++ Models/GeneralSettings.cs
@@ -6,7 +6,9 @@
         public static DateTime DataRangeTo = default(DateTime);
         public static string Currency = null;
         public static DateTime DeleteOldTo = default(DateTime);
-
+        public static List<Transaction> Transactions = new List<Transaction>();
+        public static List<HistoryItem> history = new List<HistoryItem>();
+        // we will not delete anything from the history
         public static void SetAllNull()
         {
             GeneralSettings.DataRangeFrom = default(DateTime);
@@ -15,13 +17,60 @@
             GeneralSettings.DeleteOldTo = default(DateTime);
         }
 
-        //public static void SetValues(DateTime DataRangeFrom, DateTime DateRangeTo, string Currency, DateTime DeleteOldTo)
-        //{
+        public static void SetTransactions()
+        {
+            Appdbcontxt _context = new Appdbcontxt();
+            Transactions = _context.Transactions.Where(t => t.UserId == SharedValues.CurUser.Id).ToList();
+        }
 
-        //    GeneralSettings.DataRangeFrom = DataRangeFrom;
-        //    GeneralSettings.DataRangeTo = DateRangeTo;
-        //    GeneralSettings.Currency = Currency;
-        //    GeneralSettings.DeleteOldTo = DeleteOldTo;
-        //}
+        public static void SetHistoryItems()
+        {
+            Appdbcontxt _context = new Appdbcontxt();
+            history = _context.History.Where(t => t.UserId == SharedValues.CurUser.Id).ToList();
+        }
+
+        public static void UpdateTransactions()
+        {
+            List<Transaction> NewTransactions = new List<Transaction>();
+            foreach (var transaction in GeneralSettings.Transactions)
+            {
+                if (transaction.Timestamp <= GeneralSettings.DeleteOldTo)
+                {
+                    NewTransactions.Add(transaction);
+                }
+            }
+            using (var _context = new Appdbcontxt())
+            {
+                _context.Transactions.RemoveRange(GeneralSettings.Transactions);
+                _context.SaveChanges();
+
+                _context.Transactions.AddRange(NewTransactions);
+                _context.SaveChanges();
+
+                GeneralSettings.Transactions = NewTransactions;
+            }
+        }
+
+        public static void UpdateHistory()
+        {
+            List<HistoryItem> NewHistory = new List<HistoryItem>();
+            foreach (var hstory in GeneralSettings.history)
+            {
+                if (hstory.Timestamp <= GeneralSettings.DeleteOldTo)
+                {
+                    NewHistory.Add(hstory);
+                }
+            }
+            using (var _context = new Appdbcontxt())
+            {
+                _context.History.RemoveRange(GeneralSettings.history);
+                _context.SaveChanges();
+
+                _context.History.AddRange(NewHistory);
+                _context.SaveChanges();
+                GeneralSettings.history = NewHistory;
+            }
+        }
+
     }
 }
