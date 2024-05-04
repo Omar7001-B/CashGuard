e2c0a2acbcbd94009dce37b77370eaa0933423b5
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-05-04 23:45:32 +0300

    Js fiels for the new views

---
+++ wwwroot/js/UserExist.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    setTimeout(function () {
+        $('body').removeClass('loading');
+    }, 1000);
+
+    // Function to handle button click event
+    $('.go-back-btn').on('click', function () {
+        window.location.href = "/Login/index"; // Replace "your_specific_url_here" with the URL you want to go back to
+    });
+});
