9428fcd3e06254739d0f8f086fb1b9c966968e79
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-05-04 22:37:02 +0300

    Migrations for delete the Bank account and added Gender

---
+++ Migrations/20240504190400_init1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ThreeFriends.Migrations
+{
+    public partial class init1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bank_Account_ID",
+                table: "User");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "User",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "User");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Bank_Account_ID",
+                table: "User",
+                type: "TEXT",
+                maxLength: 50,
+                nullable: true);
+        }
+    }
+}
