a2795cc9eab4beed15354b1e2aa6884af4e64a36
Omar Abbas <Omar.20375710@compit.aun.edu.eg>
2024-05-05 17:39:51 +0300

    Added Semi Sort Function (Whenn press on the arrow)

--- wwwroot/js/DashBoard.js
+++ wwwroot/js/DashBoard.js
@@ -221,3 +221,27 @@ const downloadFile = function (data, fileType, fileName = '') {
     a.click();
     a.remove();
 }
+
+
+function sortTable(columnIndex) {
+            var table, rows, switching, i, x, y, shouldSwitch;
+            table = document.getElementById("customers_table").getElementsByTagName('table')[0];
+            switching = true;
+            while (switching) {
+                switching = false;
+                rows = table.rows;
+                for (i = 1; i < (rows.length - 1); i++) {
+                    shouldSwitch = false;
+                    x = rows[i].getElementsByTagName("td")[columnIndex];
+                    y = rows[i + 1].getElementsByTagName("td")[columnIndex];
+                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                        shouldSwitch = true;
+                        break;
+                    }
+                }
+                if (shouldSwitch) {
+                    rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                    switching = true;
+                }
+            }
+        }
