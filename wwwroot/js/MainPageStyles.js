f6294c5c34f077c1b1c7ec2497291d1395b3c777
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-05-26 17:43:52 +0300

    New SideBar added and all another views fixed and updated

--- wwwroot/js/MainPageStyles.js
+++ wwwroot/js/MainPageStyles.js
@@ -1,38 +1,24 @@
-﻿document.addEventListener("DOMContentLoaded", function (event) {
+﻿// add hovered class to selected list item
+let list = document.querySelectorAll(".navigation li");
 
-    const showNavbar = (toggleId, navId, bodyId, headerId) => {
-        const toggle = document.getElementById(toggleId),
-            nav = document.getElementById(navId),
-            bodypd = document.getElementById(bodyId),
-            headerpd = document.getElementById(headerId)
+function activeLink() {
+    list.forEach((item) => {
+        item.classList.remove("hovered");
+    });
+    this.classList.add("hovered");
+}
 
-        // Validate that all variables exist
-        if (toggle && nav && bodypd && headerpd) {
-            toggle.addEventListener('click', () => {
-                // show navbar
-                nav.classList.toggle('show')
-                // change icon
-                toggle.classList.toggle('bx-x')
-                // add padding to body
-                bodypd.classList.toggle('body-pd')
-                // add padding to header
-                headerpd.classList.toggle('body-pd')
-            })
-        }
-    }
+list.forEach((item) => item.addEventListener("mouseover", activeLink));
 
-    showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header')
+// Menu Toggle
+let toggle = document.querySelector(".toggle");
+let navigation = document.querySelector(".navigation");
+let main = document.querySelector(".main");
 
-    /*===== LINK ACTIVE =====*/
-    const linkColor = document.querySelectorAll('.nav_link')
+toggle.onclick = function () {
+    navigation.classList.toggle("active");
+    main.classList.toggle("active");
+};
 
-    function colorLink() {
-        if (linkColor) {
-            linkColor.forEach(l => l.classList.remove('active'))
-            this.classList.add('active')
-        }
-    }
-    linkColor.forEach(l => l.addEventListener('click', colorLink))
 
-    // Your code to run since DOM is loaded and ready
-});
+
